b1b5f2691160c895797c83062f2d3857209cc832
rjx5062 <rishitajain2023@u.northwestern.edu>
2023-08-30 16:20:20 -0700

    Leetcode Question 88

---
+++ Question-88/BruteForce-Solution.py
@@ -0,0 +1,58 @@
+"""
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the
+number of elements in nums1 and nums2 respectively. Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate 
+this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n 
+elements are set to 0 and should be ignored. nums2 has a length of n.
+
+"""
+"""
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+"""
+
+
+
+
+from typing import *
+class Solution:
+
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+
+        del nums1[m:(m + n)]
+        # print(nums1)
+        if len(nums1) == 0:
+            for i in range(0, len(nums2)):
+                nums1.append(nums2[i])
+        else:
+            for i in range(0, len(nums2)):
+                for j in range(0, len(nums1)):
+                    if nums2[i] < nums1[j]:
+                        # print(i, j, nums2[i], nums1[j])
+                        nums1.insert(j, nums2[i])
+                        break
+                    elif j == len(nums1) - 1:
+                        nums1.append(nums2[i])
+                        break
+
+        print(nums1)
+
+
+if __name__ == "__main__":
+    s1 = Solution()
+    s1.merge([2, 0], 1, [1], 1)
